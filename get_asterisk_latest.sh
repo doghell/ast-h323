ac5b286b0c4e25341b868d3c5930a592710dc0b3
d.kochmashev <d.kochmashev@gmail.com>
2012-05-25 08:39:51 +0000

    Fixed incorrect path, moved rm call to right place

--- get_asterisk_latest.sh
+++ get_asterisk_latest.sh
@@ -1,8 +1,8 @@
 [ -h asterisk ] && rm -vf asterisk
 [ -d asterisk-svn_latest ] && rm -vrf asterisk-svn_latest
 svn checkout http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_latest || exit 1
-rm -vf asterisk/configure || exit 1
 if [ -d asterisk-svn_latest ]; then
+	rm -vf asterisk-svn_latest/configure || exit 1
 	ln -svnf asterisk-svn_latest asterisk
 else
 	exit 1
