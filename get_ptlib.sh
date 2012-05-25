feed33f1928e98d4b2875441b8408b971987eb7d
d.kochmashev <d.kochmashev@gmail.com>
2012-05-25 06:35:03 +0000

    get_* scripts update

--- get_ptlib.sh
+++ get_ptlib.sh
@@ -1,7 +1,12 @@
-[ -h ptlib ] && exit
-if [ ! -f ptlib-2.10.5.tar.bz2 ]; then
-	[ -d ptlib-2.10.5 ] && rm -vrf ptlib-2.10.5
-	wget http://downloads.sourceforge.net/project/opalvoip/v3.10%20Luyten/Stable%205/ptlib-2.10.5.tar.bz2 && \
-	tar xvjf ptlib-2.10.5.tar.bz2
+[ -h ptlib ] && echo "Already exists" && exit
+if [ ! -d ptlib-2.10.5 ]; then
+	if [ ! -f ptlib-2.10.5.tar.bz2 ]; then
+		wget http://downloads.sourceforge.net/project/opalvoip/v3.10%20Luyten/Stable%205/ptlib-2.10.5.tar.bz2 || exit 1
+	fi
+	tar xvjf ptlib-2.10.5.tar.bz2 || exit 1
 fi
-[ -d ptlib-2.10.5 ] && ln -svnf ptlib-2.10.5 ptlib
+if [ -d ptlib-2.10.5 ]; then
+	ln -svnf ptlib-2.10.5 ptlib
+else
+	exit 1
+fi
--- get_spandsp.sh
+++ get_spandsp.sh
@@ -1,8 +1,13 @@
-[ -h spandsp ] && exit
-if [ ! -f spandsp-0.0.6pre20.tgz ]; then
-	[ -d spandsp-0.0.6pre20 ] && rm -vrf spandsp-0.0.6pre20
-	wget http://www.soft-switch.org/downloads/spandsp/spandsp-0.0.6pre20.tgz && \
-	tar xvzf spandsp-0.0.6pre20.tgz && \
-	mv -vf spandsp-0.0.6 spandsp-0.0.6pre20
+[ -h spandsp ] && echo "Already exists" && exit
+if [ ! -d spandsp-0.0.6pre20 ]; then
+	if [ ! -f spandsp-0.0.6pre20.tgz ]; then
+		wget http://www.soft-switch.org/downloads/spandsp/spandsp-0.0.6pre20.tgz || exit 1
+	fi
+	tar xvzf spandsp-0.0.6pre20.tgz || exit 1
+	mv -vf spandsp-0.0.6 spandsp-0.0.6pre20 || exit 1
 fi
-[ -d spandsp-0.0.6pre20 ] && ln -svnf spandsp-0.0.6pre20 spandsp
+if [ -d spandsp-0.0.6pre20 ]; then
+	ln -svnf spandsp-0.0.6pre20 spandsp
+else
+	exit 1
+fi
