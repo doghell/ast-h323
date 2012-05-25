eb81f4585372a3a947344587b0254528a90c6287
d.kochmashev <d.kochmashev@gmail.com>
2012-05-25 08:41:21 +0000

    Update to use Asterisk SVN r367780

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r367559 ]; then
-	svn checkout http://svn.asterisk.org/svn/asterisk/trunk@367559 asterisk-svn_r367559 || exit 1
-	rm -vf asterisk-svn_r367559/configure || exit 1
+if [ ! -d asterisk-svn_r367780 ]; then
+	svn checkout http://svn.asterisk.org/svn/asterisk/trunk@367780 asterisk-svn_r367780 || exit 1
+	rm -vf asterisk-svn_r367780/configure || exit 1
 fi
-if [ -d asterisk-svn_r367559 ]; then
-	ln -svnf asterisk-svn_r367559 asterisk
+if [ -d asterisk-svn_r367780 ]; then
+	ln -svnf asterisk-svn_r367780 asterisk
 else
 	exit 1
 fi
