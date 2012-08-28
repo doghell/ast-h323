b76e846b9b113c7ee810a870419d5f9d4ea72c63
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-08-28 09:58:59 +0000

    Added build script existence check

--- trunk/build-debug.sh
+++ trunk/build-debug.sh
@@ -6,7 +6,7 @@ PACKAGE_LIST=$1
 [ -n "${PACKAGE_LIST}" ] || PACKAGE_LIST=`cat ${SRC_BASE}/package_list`
 
 for PACKAGE in ${PACKAGE_LIST}; do
-	if [ -h "${PACKAGE}" ]; then
+	if [ -h "${PACKAGE}" -a -x "${SRC_BASE}/build_${PACKAGE}.sh" ]; then
 		echo -e "\nBuilding '${PACKAGE}'...\n"
 		${SRC_BASE}/build_${PACKAGE}.sh debug ${HOME}
 	fi
