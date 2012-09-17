48290d69671e6ad528d0ddc0be5ff38b2a1f88e4
d.kochmashev <d.kochmashev@gmail.com>
2012-09-17 06:09:42 +0000

    Updated FreeBSD build

--- build_spandsp.sh
+++ build_spandsp.sh
@@ -4,6 +4,10 @@ SRC_BASE=`dirname $0`
 [ -f "${SRC_BASE}/$0" ] || SRC_BASE=`dirname $0`
 [ -n "${2}" ] && INSTALL_PREFIX="--prefix=${2}"
 
+if [ "`uname`" == "FreeBSD" ]; then
+	export CFLAGS="-I/usr/local/include"
+	export LDFLAGS="-L/usr/local/lib"
+fi
 export PATH="${HOME}/bin:${PATH}"
 
 cd "${SRC_BASE}/spandsp"
