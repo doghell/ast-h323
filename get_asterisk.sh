d87e033eccb0cf4189262f4e5c64da1b45029ef3
d.kochmashev <d.kochmashev@gmail.com>
2012-09-21 05:19:34 +0000

    Added branch for Asterisk 1.8

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,12 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r373201 ]; then
-	svn checkout http://svn.asterisk.org/svn/asterisk/trunk@373201 asterisk-svn_r373201 || exit 1
-	rm -vf asterisk-svn_r373201/configure || exit 1
+if [ ! -d asterisk-1.8.16.0 ]; then
+	if [ ! -f asterisk-1.8.16.0.tar.gz ]; then
+		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-1.8.16.0.tar.gz || exit 1
+	fi
+	tar xvzf asterisk-1.8.16.0.tar.gz || exit 1
 fi
-if [ -d asterisk-svn_r373201 ]; then
-	ln -svnf asterisk-svn_r373201 asterisk
+if [ -d asterisk-1.8.16.0 ]; then
+	ln -svnf asterisk-1.8.16.0 asterisk
 else
 	exit 1
 fi
