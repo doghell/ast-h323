aa0aefdb2b30fe798c23b7d27bf1b7749b070d82
d.kochmashev <d.kochmashev@gmail.com>
2012-09-25 07:05:26 +0000

    Fixed usage of unavailable var

--- patch_asterisk.sh
+++ patch_asterisk.sh
@@ -5,5 +5,5 @@ PACKAGE_LIST=$1
 [ -f "${SRC_BASE}/$0" ] || SRC_BASE=`dirname $0`
 
 cd "${SRC_BASE}/asterisk"
-echo -e "\nPatching '${PACKAGE}'...\n"
+echo -e "\nPatching 'asterisk'...\n"
 patch -bENp0 < "${SRC_BASE}/asterisk-patchset/patchset.diff"
