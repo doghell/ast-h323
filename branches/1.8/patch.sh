b24a755077cfadae5886ef46ad7bb4294c50ec2d
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-09-25 05:10:30 +0000

    Changed patch set format

--- branches/1.8/patch.sh
+++ branches/1.8/patch.sh
@@ -10,14 +10,19 @@ fi
 for PACKAGE in ${PACKAGE_LIST}; do
 	[ -d "${SRC_BASE}/${PACKAGE}" ] || exit 1
 	if [ -d "${SRC_BASE}/${PACKAGE}-patchset" -a ! -f "${SRC_BASE}/${PACKAGE}/.patchset_applied" ]; then
-		echo -e "\nCopying patch set for '${PACKAGE}'...\n"
-		cp -vrf ${SRC_BASE}/${PACKAGE}-patchset/* ${SRC_BASE}/${PACKAGE}
-		echo -e "\nPatching '${PACKAGE}'...\n"
-		for PATCH in `find ${SRC_BASE}/${PACKAGE}/ -name "*.patch"`; do
-			ORIGINAL=`echo ${PATCH} | sed -r 's/\.patch$//'`
-			[ -f "${ORIGINAL}" ] || touch ${ORIGINAL}
-			patch -bNp0 ${ORIGINAL} < ${PATCH}
-		done
+		if [ -x "${SRC_BASE}/patch_${PACKAGE}.sh" ]; then
+			${SRC_BASE}/patch_${PACKAGE}.sh || exit 1
+		else
+			echo -e "\nCopying patch set for '${PACKAGE}'...\n"
+			cd ${SRC_BASE}/${PACKAGE}-patchset
+			tar cvf - `find . -type f -name "*.patch"` | tar xvf - -C ${SRC_BASE}/${PACKAGE}
+			echo -e "\nPatching '${PACKAGE}'...\n"
+			for PATCH in `find ${SRC_BASE}/${PACKAGE}/ -name "*.patch"`; do
+				ORIGINAL=`echo ${PATCH} | sed -r 's/\.patch$//'`
+				[ -f "${ORIGINAL}" ] || touch ${ORIGINAL}
+				patch -bNp0 ${ORIGINAL} < ${PATCH} || exit 1
+			done
+		fi
 		touch "${SRC_BASE}/${PACKAGE}/.patchset_applied"
 	fi
 done
