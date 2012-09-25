080652035d658c575359d93db64811254a8db91c
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-09-25 07:02:23 +0000

    Reduced extended package list

--- branches/10/get_opal_latest.sh
+++
@@ -1,8 +0,0 @@
-[ -h opal ] && rm -vf opal
-[ -d opal-svn_latest ] && rm -vrf opal-svn_latest
-svn checkout http://opalvoip.svn.sourceforge.net/svnroot/opalvoip/opal/trunk opal-svn_latest || exit 1
-if [ -d opal-svn_latest ]; then
-	ln -svnf opal-svn_latest opal
-else
-	exit 1
-fi
