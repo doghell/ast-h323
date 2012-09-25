9dbd0768d2d9f366ef55637df66cd4f5d21fc54e
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-09-25 06:56:19 +0000

    Improved compatibility with older Subversion releases

--- trunk/get_asterisk.sh
+++ trunk/get_asterisk.sh
@@ -1,6 +1,6 @@
 [ -h asterisk ] && echo "Already exists" && exit
 if [ ! -d asterisk-svn_r373402 ]; then
-	svn checkout http://svn.asterisk.org/svn/asterisk/trunk@373402 asterisk-svn_r373402 || exit 1
+	svn checkout -r 373402 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r373402 || exit 1
 	rm -vf asterisk-svn_r373402/configure || exit 1
 fi
 if [ -d asterisk-svn_r373402 ]; then
