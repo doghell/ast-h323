e0a88a87016f8afb9fe223fd3397fd020a47f639
d.kochmashev <d.kochmashev@gmail.com>
2012-09-25 05:10:30 +0000

    Changed patch set format

---
+++ patch_asterisk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SRC_BASE=`pwd`
+PACKAGE_LIST=$1
+[ -f "${SRC_BASE}/$0" ] || SRC_BASE=`dirname $0`
+
+cd "${SRC_BASE}/asterisk"
+echo -e "\nPatching '${PACKAGE}'...\n"
+patch -bNp0 < "${SRC_BASE}/asterisk-patchset/patchset.diff"
