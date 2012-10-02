531478d3683b41332b911e24e3575d1ff1ef1b21
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-10-02 05:40:06 +0000

    Updated to use Asterisk SVN r374206

--- trunk/get_asterisk.sh
+++ trunk/get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r374095 ]; then
-	svn checkout -r 374095 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r374095 || exit 1
-	rm -vf asterisk-svn_r374095/configure || exit 1
+if [ ! -d asterisk-svn_r374206 ]; then
+	svn checkout -r 374206 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r374206 || exit 1
+	rm -vf asterisk-svn_r374206/configure || exit 1
 fi
-if [ -d asterisk-svn_r374095 ]; then
-	ln -svnf asterisk-svn_r374095 asterisk
+if [ -d asterisk-svn_r374206 ]; then
+	ln -svnf asterisk-svn_r374206 asterisk
 else
 	exit 1
 fi
