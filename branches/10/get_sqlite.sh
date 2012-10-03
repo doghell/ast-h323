abcc6eef3b4a17e8d258eeca982ff223e3b3549f
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-10-03 10:02:23 +0000

    Added pkg-config and sqlite version checks

---
+++ branches/10/get_sqlite.sh
@@ -0,0 +1,14 @@
+[ -h pkg-config ] && echo "Already exists" && exit
+sqlite -version | perl -e 'if (<> =~ /^([\d\.]+)/) { @v = split /\./, $1; $ver = $v[0]*10000 + $v[1]*100 +$v[2]; exit 0 unless $ver < 30714; } exit 1;'
+[ $? -eq 0 ] && exit 0
+if [ ! -d sqlite-autoconf-3071400 ]; then
+	if [ ! -f sqlite-autoconf-3071400.tar.gz ]; then
+		wget http://www.sqlite.org/sqlite-autoconf-3071400.tar.gz || exit 1
+	fi
+	tar xvzf sqlite-autoconf-3071400.tar.gz || exit 1
+fi
+if [ -d sqlite-autoconf-3071400 ]; then
+	ln -svnf sqlite-autoconf-3071400 sqlite
+else
+	exit 1
+fi
