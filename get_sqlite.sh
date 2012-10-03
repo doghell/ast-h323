865c3acb2f2ee0e75982d1868dcdc35711f789c5
d.kochmashev <d.kochmashev@gmail.com>
2012-10-03 11:08:02 +0000

    Corrected sqlite3 download script

--- get_sqlite.sh
+++ get_sqlite.sh
@@ -1,5 +1,5 @@
-[ -h pkg-config ] && echo "Already exists" && exit
-sqlite -version | perl -e 'if (<> =~ /^([\d\.]+)/) { @v = split /\./, $1; $ver = $v[0]*10000 + $v[1]*100 +$v[2]; exit 0 unless $ver < 30714; } exit 1;'
+[ -h sqlite ] && echo "Already exists" && exit
+sqlite3 -version | perl -e 'if (<> =~ /^([\d\.]+)/) { @v = split /\./, $1; $ver = $v[0]*10000 + $v[1]*100 +$v[2]; exit 0 unless $ver < 30714; } exit 1;'
 [ $? -eq 0 ] && exit 0
 if [ ! -d sqlite-autoconf-3071400 ]; then
 	if [ ! -f sqlite-autoconf-3071400.tar.gz ]; then
