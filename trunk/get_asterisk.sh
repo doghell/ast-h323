dea000f733b809d8ded687066e555535f651d879
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-10-08 08:19:44 +0000

    Updated to use Asterisk SVN r374651 + commented unused capabilities

--- trunk/get_asterisk.sh
+++ trunk/get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r374523 ]; then
-	svn checkout -r 374523 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r374523 || exit 1
-	rm -vf asterisk-svn_r374523/configure || exit 1
+if [ ! -d asterisk-svn_r374651 ]; then
+	svn checkout -r 374651 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r374651 || exit 1
+	rm -vf asterisk-svn_r374651/configure || exit 1
 fi
-if [ -d asterisk-svn_r374523 ]; then
-	ln -svnf asterisk-svn_r374523 asterisk
+if [ -d asterisk-svn_r374651 ]; then
+	ln -svnf asterisk-svn_r374651 asterisk
 else
 	exit 1
 fi
