a6b353324d37d14d99b6d6a6cc7cb995b1419118
d.kochmashev <d.kochmashev@gmail.com>
2012-10-09 05:34:40 +0000

    Corrected address family definition in __oh323_udptl_create. Updated to use Asterisk 1.8.17.0, 10.9.0 and SVN r374753.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r374651 ]; then
-	svn checkout -r 374651 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r374651 || exit 1
-	rm -vf asterisk-svn_r374651/configure || exit 1
+if [ ! -d asterisk-svn_r374753 ]; then
+	svn checkout -r 374753 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r374753 || exit 1
+	rm -vf asterisk-svn_r374753/configure || exit 1
 fi
-if [ -d asterisk-svn_r374651 ]; then
-	ln -svnf asterisk-svn_r374651 asterisk
+if [ -d asterisk-svn_r374753 ]; then
+	ln -svnf asterisk-svn_r374753 asterisk
 else
 	exit 1
 fi
