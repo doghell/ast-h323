327c6a54986efc4ffbdb79e2fb9aef024be66dc2
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-10-11 06:18:06 +0000

    Fixed incorrect directory and file names

--- trunk/get_asterisk-g72x.sh
+++ trunk/get_asterisk-g72x.sh
@@ -1,7 +1,7 @@
 [ -h asterisk-g72x ] && echo "Already exists" && exit
-if [ ! -d asterisk-g72x_trunk ]; then
-	mkdir -vp asterisk-g72x_trunk && \
-	wget --directory-prefix=asterisk-g72x_trunk \
+if [ ! -d asterisk-g72x_110 ]; then
+	mkdir -vp asterisk-g72x_110 && \
+	wget --directory-prefix=asterisk-g72x_110 \
 		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-pentium4.so \
 		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-pentium4-sse3.so \
 		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-x86_64-pentium4.so \
@@ -21,8 +21,8 @@ if [ ! -d asterisk-g72x_trunk ]; then
 		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-x86_64-opteron.so \
 		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-x86_64-opteron-sse3.so
 fi
-if [ -d asterisk-g72x_trunk ]; then
-	ln -svnf asterisk-g72x_trunk asterisk-g72x
+if [ -d asterisk-g72x_110 ]; then
+	ln -svnf asterisk-g72x_110 asterisk-g72x
 else
 	exit 1
 fi
