c69aba8bf63aba066632fac2a52118f2b4232896
d.kochmashev <d.kochmashev@gmail.com>
2012-10-11 05:26:18 +0000

    Updated to use H323Plus CVS 2012-10-11 10:40, Asterisk SVN r374832, GNU Gatekeeper e20121004_2 (extended package set)

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r374790 ]; then
-	svn checkout -r 374790 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r374790 || exit 1
-	rm -vf asterisk-svn_r374790/configure || exit 1
+if [ ! -d asterisk-svn_r374832 ]; then
+	svn checkout -r 374832 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r374832 || exit 1
+	rm -vf asterisk-svn_r374832/configure || exit 1
 fi
-if [ -d asterisk-svn_r374790 ]; then
-	ln -svnf asterisk-svn_r374790 asterisk
+if [ -d asterisk-svn_r374832 ]; then
+	ln -svnf asterisk-svn_r374832 asterisk
 else
 	exit 1
 fi
