a439b8c7e1fd89fbac18f35034b785a93d34efc1
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-10-17 10:29:43 +0000

    Updated to use H323Plus CVS 2012-10-17 16:18

--- branches/1.8/get_h323plus.sh
+++ branches/1.8/get_h323plus.sh
@@ -1,10 +1,10 @@
 [ -h h323plus ] && echo "Already exists" && exit
-if [ ! -d h323plus-cvs_20121011-1040 ]; then
-	cvs -z3 -d:pserver:anonymous@h323plus.cvs.sourceforge.net:/cvsroot/h323plus co -P -D "2012-10-11 10:40" h323plus || exit 1
-	mv h323plus h323plus-cvs_20121011-1040 || exit 1
+if [ ! -d h323plus-cvs_20121017-1618 ]; then
+	cvs -z3 -d:pserver:anonymous@h323plus.cvs.sourceforge.net:/cvsroot/h323plus co -P -D "2012-10-17 16:18" h323plus || exit 1
+	mv h323plus h323plus-cvs_20121017-1618 || exit 1
 fi
-if [ -d h323plus-cvs_20121011-1040 ]; then
-	ln -svnf h323plus-cvs_20121011-1040 h323plus
+if [ -d h323plus-cvs_20121017-1618 ]; then
+	ln -svnf h323plus-cvs_20121017-1618 h323plus
 else
 	exit 1
 fi
