17ec41fafdb6f6db8fd9fb552c380896ada6c66f
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-10-18 15:35:08 +0000

    Updated to use PTLib 2.10.8 and Asterisk SVN r375188

--- trunk/get_asterisk.sh
+++ trunk/get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r375136 ]; then
-	svn checkout -r 375136 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r375136 || exit 1
-	rm -vf asterisk-svn_r375136/configure || exit 1
+if [ ! -d asterisk-svn_r375188 ]; then
+	svn checkout -r 375188 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r375188 || exit 1
+	rm -vf asterisk-svn_r375188/configure || exit 1
 fi
-if [ -d asterisk-svn_r375136 ]; then
-	ln -svnf asterisk-svn_r375136 asterisk
+if [ -d asterisk-svn_r375188 ]; then
+	ln -svnf asterisk-svn_r375188 asterisk
 else
 	exit 1
 fi
