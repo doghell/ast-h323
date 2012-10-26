92af698903b400373542b9121c5af10e384caba8
d.kochmashev <d.kochmashev@gmail.com>
2012-10-26 12:40:05 +0000

    Updated to use PTLib 2.10.8 from SVN repository, H323Plus CVS 2012-10-26 18:25 and Asterisk SVN r375354. Removed unused scripts.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r375188 ]; then
-	svn checkout -r 375188 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r375188 || exit 1
-	rm -vf asterisk-svn_r375188/configure || exit 1
+if [ ! -d asterisk-svn_r375354 ]; then
+	svn checkout -r 375354 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r375354 || exit 1
+	rm -vf asterisk-svn_r375354/configure || exit 1
 fi
-if [ -d asterisk-svn_r375188 ]; then
-	ln -svnf asterisk-svn_r375188 asterisk
+if [ -d asterisk-svn_r375354 ]; then
+	ln -svnf asterisk-svn_r375354 asterisk
 else
 	exit 1
 fi
