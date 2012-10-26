1be40dc5a4faf51e046b59fdde8d451c0e9bb047
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-10-26 12:40:05 +0000

    Updated to use PTLib 2.10.8 from SVN repository, H323Plus CVS 2012-10-26 18:25 and Asterisk SVN r375354. Removed unused scripts.

--- trunk/get_ptlib.sh
+++ trunk/get_ptlib.sh
@@ -1,9 +1,6 @@
 [ -h ptlib ] && echo "Already exists" && exit
 if [ ! -d ptlib-2.10.8 ]; then
-	if [ ! -f ptlib-2.10.8.tar.bz2 ]; then
-		wget http://downloads.sourceforge.net/project/opalvoip/v3.10%20Luyten/Stable%208/ptlib-2.10.8.tar.bz2 || exit 1
-	fi
-	tar xvjf ptlib-2.10.8.tar.bz2 || exit 1
+	svn co http://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/tags/v2_10_8 ptlib-2.10.8 || exit 1
 fi
 if [ -d ptlib-2.10.8 ]; then
 	ln -svnf ptlib-2.10.8 ptlib
