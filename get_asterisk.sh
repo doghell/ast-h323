c093e2b00bee80532334440da0aed6ba518a10e3
d.kochmashev <d.kochmashev@gmail.com>
2012-10-31 05:51:46 +0000

    Returned back to using PTlib 2.10.7. Updated to use Asterisk SVN r375524.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r375354 ]; then
-	svn checkout -r 375354 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r375354 || exit 1
-	rm -vf asterisk-svn_r375354/configure || exit 1
+if [ ! -d asterisk-svn_r375524 ]; then
+	svn checkout -r 375524 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r375524 || exit 1
+	rm -vf asterisk-svn_r375524/configure || exit 1
 fi
-if [ -d asterisk-svn_r375354 ]; then
-	ln -svnf asterisk-svn_r375354 asterisk
+if [ -d asterisk-svn_r375524 ]; then
+	ln -svnf asterisk-svn_r375524 asterisk
 else
 	exit 1
 fi
