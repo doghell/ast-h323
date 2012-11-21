dfea496739b1c2fed683a7debfb2ec67309e0fe3
d.kochmashev <d.kochmashev@gmail.com>
2012-11-21 11:28:04 +0000

    Updated to use H323Plus CVS 2012-11-21 12:25 and Asterisk SVN r376573

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r375924 ]; then
-	svn checkout -r 375924 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r375924 || exit 1
-	rm -vf asterisk-svn_r375924/configure || exit 1
+if [ ! -d asterisk-svn_r376573 ]; then
+	svn checkout -r 376573 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r376573 || exit 1
+	rm -vf asterisk-svn_r376573/configure || exit 1
 fi
-if [ -d asterisk-svn_r375924 ]; then
-	ln -svnf asterisk-svn_r375924 asterisk
+if [ -d asterisk-svn_r376573 ]; then
+	ln -svnf asterisk-svn_r376573 asterisk
 else
 	exit 1
 fi
