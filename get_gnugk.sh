d73bb32aa5969a6d20107d1e3be843945860bcb9
d.kochmashev <d.kochmashev@gmail.com>
2012-11-23 07:50:00 +0000

    Updated to use GNU GK 2.3.2e_20121123-1 (fixed memory leak in GWRewrite Cache code)

--- get_gnugk.sh
+++ get_gnugk.sh
@@ -1,12 +1,12 @@
 [ -h gnugk ] && echo "Already exists" && exit
-if [ ! -d gnugk-2.3.2e_20121004-2 ]; then
-	if [ ! -f gnugk-2.3.2e_20121004-2.tar.bz2 ]; then
-		wget http://files.tmc.enforta.net/mmsvc/v22/pkg/gnugk-2.3.2e_20121004-2.tar.bz2
+if [ ! -d gnugk-2.3.2e_20121123-1 ]; then
+	if [ ! -f gnugk-2.3.2e_20121123-1.tar.bz2 ]; then
+		wget http://files.tmc.enforta.net/mmsvc/v22/pkg/gnugk-2.3.2e_20121123-1.tar.bz2
 	fi
-	tar xvjf gnugk-2.3.2e_20121004-2.tar.bz2 || exit 1
+	tar xvjf gnugk-2.3.2e_20121123-1.tar.bz2 || exit 1
 fi
-if [ -d gnugk-2.3.2e_20121004-2 ]; then
-	ln -svnf gnugk-2.3.2e_20121004-2 gnugk
+if [ -d gnugk-2.3.2e_20121123-1 ]; then
+	ln -svnf gnugk-2.3.2e_20121123-1 gnugk
 else
 	exit 1
 fi
