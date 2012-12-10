55036e9ea970bf83fac0f86eb3c9950d17fef38f
d.kochmashev <d.kochmashev@gmail.com>
2012-12-10 13:57:57 +0000

    Added curl. Updated to use PTLib SVN 2.10-branch, H323Plus CVS 2012-12-10 15:48 and GNU GK 2.3.2e_20121210-1. Made H323Plus patchset (corrected PTLIB_VER check).

--- get_h323plus.sh
+++ get_h323plus.sh
@@ -1,10 +1,10 @@
 [ -h h323plus ] && echo "Already exists" && exit
-if [ ! -d h323plus-cvs_20121031-2144 ]; then
-	cvs -z3 -d:pserver:anonymous@h323plus.cvs.sourceforge.net:/cvsroot/h323plus co -P -D "2012-10-31 21:44" h323plus || exit 1
-	mv h323plus h323plus-cvs_20121031-2144 || exit 1
+if [ ! -d h323plus-cvs_20121210-1548 ]; then
+	cvs -z3 -d:pserver:anonymous@h323plus.cvs.sourceforge.net:/cvsroot/h323plus co -P -D "2012-12-10 15:48" h323plus || exit 1
+	mv h323plus h323plus-cvs_20121210-1548 || exit 1
 fi
-if [ -d h323plus-cvs_20121031-2144 ]; then
-	ln -svnf h323plus-cvs_20121031-2144 h323plus
+if [ -d h323plus-cvs_20121210-1548 ]; then
+	ln -svnf h323plus-cvs_20121210-1548 h323plus
 else
 	exit 1
 fi
