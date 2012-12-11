eaeed22d6c36cac8f0cfe5aaed437973df86c08f
d.kochmashev <d.kochmashev@gmail.com>
2012-12-11 10:06:19 +0000

    Updated to use Asterisk 1.8.19.0, 10.11.0, 11.1.0 and SVN r377802.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,13 +1,13 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-1.8.18.0 ]; then
-	if [ ! -f asterisk-1.8.18.0.tar.gz ]; then
-		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-1.8.18.0.tar.gz || exit 1
+if [ ! -d asterisk-1.8.19.0 ]; then
+	if [ ! -f asterisk-1.8.19.0.tar.gz ]; then
+		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-1.8.19.0.tar.gz || exit 1
 	fi
-	tar xvzf asterisk-1.8.18.0.tar.gz || exit 1
-	rm -vf asterisk-1.8.18.0/configure
+	tar xvzf asterisk-1.8.19.0.tar.gz || exit 1
+	rm -vf asterisk-1.8.19.0/configure
 fi
-if [ -d asterisk-1.8.18.0 ]; then
-	ln -svnf asterisk-1.8.18.0 asterisk
+if [ -d asterisk-1.8.19.0 ]; then
+	ln -svnf asterisk-1.8.19.0 asterisk
 else
 	exit 1
 fi
