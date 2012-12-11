953d62bf9e4c7d1a569ebcfe6564b399e1f1008f
d.kochmashev <d.kochmashev@gmail.com>
2012-12-11 10:06:19 +0000

    Updated to use Asterisk 1.8.19.0, 10.11.0, 11.1.0 and SVN r377802.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r377255 ]; then
-	svn checkout -r 377255 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r377255 || exit 1
-	rm -vf asterisk-svn_r377255/configure || exit 1
+if [ ! -d asterisk-svn_r377802 ]; then
+	svn checkout -r 377802 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r377802 || exit 1
+	rm -vf asterisk-svn_r377802/configure || exit 1
 fi
-if [ -d asterisk-svn_r377255 ]; then
-	ln -svnf asterisk-svn_r377255 asterisk
+if [ -d asterisk-svn_r377802 ]; then
+	ln -svnf asterisk-svn_r377802 asterisk
 else
 	exit 1
 fi
--- get_h323plus.sh
+++ get_h323plus.sh
@@ -1,10 +1,10 @@
 [ -h h323plus ] && echo "Already exists" && exit
-if [ ! -d h323plus-cvs_20121121-1225 ]; then
-	cvs -z3 -d:pserver:anonymous@h323plus.cvs.sourceforge.net:/cvsroot/h323plus co -P -D "2012-11-21 12:25" h323plus || exit 1
-	mv h323plus h323plus-cvs_20121121-1225 || exit 1
+if [ ! -d h323plus-cvs_20121210-1548 ]; then
+	cvs -z3 -d:pserver:anonymous@h323plus.cvs.sourceforge.net:/cvsroot/h323plus co -P -D "2012-12-10 15:48" h323plus || exit 1
+	mv h323plus h323plus-cvs_20121210-1548 || exit 1
 fi
-if [ -d h323plus-cvs_20121121-1225 ]; then
-	ln -svnf h323plus-cvs_20121121-1225 h323plus
+if [ -d h323plus-cvs_20121210-1548 ]; then
+	ln -svnf h323plus-cvs_20121210-1548 h323plus
 else
 	exit 1
 fi
