1d52e0343af77c656f733f3b45cadff0bb3f5f57
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-12-13 11:08:12 +0000

    Fixed G.72x codec installation script.

--- branches/11/get_asterisk-g72x.sh
+++ branches/11/get_asterisk-g72x.sh
@@ -1,28 +1,28 @@
 [ -h asterisk-g72x ] && echo "Already exists" && exit
-if [ ! -d asterisk-g72x_100 ]; then
-	mkdir -vp asterisk-g72x_100 && \
-	wget --directory-prefix=asterisk-g72x_100 \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-pentium4.so \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-pentium4-sse3.so \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-x86_64-pentium4.so \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-x86_64-core2.so \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-x86_64-core2-sse4.so \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-opteron.so \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-opteron-sse3.so \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-x86_64-opteron.so \
-		http://asterisk.hosting.lv/bin/codec_g723-ast100-gcc4-glibc-x86_64-opteron-sse3.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-pentium4.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-pentium4-sse3.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-x86_64-pentium4.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-x86_64-core2.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-x86_64-core2-sse4.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-opteron.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-opteron-sse3.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-x86_64-opteron.so \
-		http://asterisk.hosting.lv/bin/codec_g729-ast100-gcc4-glibc-x86_64-opteron-sse3.so
+if [ ! -d asterisk-g72x_110 ]; then
+	mkdir -vp asterisk-g72x_110 && \
+	wget --directory-prefix=asterisk-g72x_110 \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-pentium4.so \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-pentium4-sse3.so \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-x86_64-pentium4.so \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-x86_64-core2.so \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-x86_64-core2-sse4.so \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-opteron.so \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-opteron-sse3.so \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-x86_64-opteron.so \
+		http://asterisk.hosting.lv/bin/codec_g723-ast110-gcc4-glibc-x86_64-opteron-sse3.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-pentium4.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-pentium4-sse3.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-x86_64-pentium4.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-x86_64-core2.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-x86_64-core2-sse4.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-opteron.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-opteron-sse3.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-x86_64-opteron.so \
+		http://asterisk.hosting.lv/bin/codec_g729-ast110-gcc4-glibc-x86_64-opteron-sse3.so
 fi
-if [ -d asterisk-g72x_100 ]; then
-	ln -svnf asterisk-g72x_100 asterisk-g72x
+if [ -d asterisk-g72x_110 ]; then
+	ln -svnf asterisk-g72x_110 asterisk-g72x
 else
 	exit 1
 fi
