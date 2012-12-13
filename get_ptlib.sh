7df558f40155521837529688b8e0e89a340af104
d.kochmashev <d.kochmashev@gmail.com>
2012-12-13 02:00:25 +0000

    Moved back to using PTLib SVN 2.8 branch. PTLib 2.10 produces memory leak in GNU GK.

--- get_ptlib.sh
+++ get_ptlib.sh
@@ -1,9 +1,10 @@
 [ -h ptlib ] && echo "Already exists" && exit
-if [ ! -d ptlib-svn_2.10 ]; then
-	svn checkout http://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/branches/v2_10 ptlib-svn_2.10 || exit 1
+if [ ! -d ptlib-svn_2.8 ]; then
+	svn checkout http://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/branches/v2_8 ptlib-svn_2.8 || exit 1
+	[ -f ptlib-svn_2.8/configure ] && rm -vf ptlib-svn_2.8/configure
 fi
-if [ -d ptlib-svn_2.10 ]; then
-	ln -svnf ptlib-svn_2.10 ptlib
+if [ -d ptlib-svn_2.8 ]; then
+	ln -svnf ptlib-svn_2.8 ptlib
 else
 	exit 1
 fi
