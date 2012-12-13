431a41a1337b1c074ba3883b2ba5add00a029d35
d.kochmashev <d.kochmashev@gmail.com>
2012-12-13 10:52:07 +0000

    Fixed Asterisk installation script. Library libasteriskssl.so was not being copied to lib directory.

--- install_asterisk.sh
+++ install_asterisk.sh
@@ -6,6 +6,11 @@ SRC_BASE=`dirname $0`
 cd "${SRC_BASE}/asterisk"
 
 cp -vf main/asterisk ${HOME}/bin
+if [ "`uname`" == "Linux" ]; then
+	cp -vf --no-dereference main/libasteriskssl.so* ${HOME}/lib
+else
+	cp -vfR main/libasteriskssl.so* ${HOME}/lib
+fi
 mkdir -p ${HOME}/lib/asterisk/modules
 for MODNAME_PFX in app bridge cdr chan codec format func pbx res ; do
 	find . -name "${MODNAME_PFX}_*.so" -exec cp -vf \{} ${HOME}/lib/asterisk/modules \;
