2704639660760b81e17a308935a19031e6a7b058
d.kochmashev <d.kochmashev@gmail.com>
2012-12-15 15:30:28 +0000

    Fixed PTLib get/build scripts.

--- get_ptlib.sh
+++ get_ptlib.sh
@@ -1,7 +1,6 @@
 [ -h ptlib ] && echo "Already exists" && exit
 if [ ! -d ptlib-svn_2.8 ]; then
 	svn checkout http://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/branches/v2_8 ptlib-svn_2.8 || exit 1
-	[ -f ptlib-svn_2.8/configure ] && rm -vf ptlib-svn_2.8/configure
 fi
 if [ -d ptlib-svn_2.8 ]; then
 	ln -svnf ptlib-svn_2.8 ptlib
