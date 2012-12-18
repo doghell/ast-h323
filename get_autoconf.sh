9518b33afff1cc36a4b5ce429b9aa579db9a7c41
d.kochmashev <d.kochmashev@gmail.com>
2012-12-18 04:47:45 +0000

    Removed version checks from "get_*" scripts.

--- get_autoconf.sh
+++ get_autoconf.sh
@@ -1,6 +1,3 @@
-[ -h autoconf ] && echo "Already exists" && exit
-autoconf -V | perl -e 'if (<> =~ /autoconf \(GNU Autoconf\) ([\d\.]+)/) { @v = split /\./, $1; exit 0 unless $v[0] < 2 || $v[1] < 69; } exit 1;'
-[ $? -eq 0 ] && exit 0
 if [ ! -d autoconf-2.69 ]; then
 	if [ ! -f autoconf-2.69.tar.gz ]; then
 		wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz || exit 1
--- get_m4.sh
+++ get_m4.sh
@@ -1,6 +1,3 @@
-[ -h m4 ] && echo "Already exists" && exit
-m4 --version | head -1 | perl -e 'if (<> =~ /\(?GNU M4\)? ([\d\.]+)/) { @v = split /\./, $1; $ver = $v[0]*10000 + $v[1]*100 + $v[2]; exit 0 unless $ver < 10416; } exit 1;'
-[ $? -eq 0 ] && exit 0
 if [ ! -d m4-1.4.16 ]; then
 	if [ ! -f m4-1.4.16.tar.bz2 ]; then
 		wget http://ftp.gnu.org/gnu/m4/m4-1.4.16.tar.bz2 || exit 1
