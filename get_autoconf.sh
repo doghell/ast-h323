5e2fe5fa4931447cde6903054365f3dd76f9e558
d.kochmashev <d.kochmashev@gmail.com>
2012-12-18 04:47:45 +0000

    Removed version checks from "get_*" scripts.

--- get_autoconf.sh
+++ get_autoconf.sh
@@ -1,6 +1,3 @@
-[ -h autoconf ] && echo "Already exists" && exit
-autoconf -V | perl -e 'if (<> =~ /autoconf \(GNU Autoconf\) ([\d\.]+)/) { @v = split /\./, $1; $ver = $v[0]*10000 + $v[1]*100; exit 0 unless $ver < 26900; } exit 1;'
-[ $? -eq 0 ] && exit 0
 if [ ! -d autoconf-2.69 ]; then
 	if [ ! -f autoconf-2.69.tar.gz ]; then
 		wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz || exit 1
