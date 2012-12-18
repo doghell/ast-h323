990fbf7e9f8a667a8c1b6b214b691ed3a496c7e2
d.kochmashev <d.kochmashev@gmail.com>
2012-12-18 04:47:45 +0000

    Removed version checks from "get_*" scripts.

--- get_pkg-config.sh
+++ get_pkg-config.sh
@@ -1,6 +1,3 @@
-[ -h pkg-config ] && echo "Already exists" && exit
-pkg-config --version | perl -e 'if (<> =~ /^([\d\.]+)/) { @v = split /\./, $1; $ver = $v[0]*10000 + $v[1]*100; exit 0 unless $ver < 2600; } exit 1;'
-[ $? -eq 0 ] && exit 0
 if [ ! -d pkg-config-0.26 ]; then
 	if [ ! -f pkg-config-0.26.tar.gz ]; then
 		wget http://pkgconfig.freedesktop.org/releases/pkg-config-0.26.tar.gz || exit 1
