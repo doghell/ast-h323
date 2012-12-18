3e677a7e1c62e1c049019875f75a4ac280fa86f9
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2012-12-18 04:47:45 +0000

    Removed version checks from "get_*" scripts.

--- branches/10/get_sqlite.sh
+++ branches/10/get_sqlite.sh
@@ -1,6 +1,3 @@
-[ -h sqlite ] && echo "Already exists" && exit
-sqlite3 -version | perl -e 'if (<> =~ /^([\d\.]+)/) { @v = split /\./, $1; $ver = $v[0]*10000 + $v[1]*100 + $v[2] + $v[3]/10; exit 0 unless $ver < 30714.1; } exit 1;'
-[ $? -eq 0 ] && exit 0
 if [ ! -d sqlite-autoconf-3071401 ]; then
 	if [ ! -f sqlite-autoconf-3071401.tar.gz ]; then
 		wget http://www.sqlite.org/sqlite-autoconf-3071401.tar.gz || exit 1
