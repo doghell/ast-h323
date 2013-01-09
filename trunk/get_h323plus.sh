f3456a73b46b2bcfed9f2ec60f739945588e9b6b
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2013-01-09 10:04:17 +0000

    Updated to use Asterisk 1.8.19.1, 10.11.1, 11.1.2 and SVN r378706. Updated to use H323Plus CVS 2013-01-09 09:48.

--- trunk/get_h323plus.sh
+++ trunk/get_h323plus.sh
@@ -1,10 +1,10 @@
 [ -h h323plus ] && echo "Already exists" && exit
-if [ ! -d h323plus-cvs_20121215-2115 ]; then
-	cvs -z3 -d:pserver:anonymous@h323plus.cvs.sourceforge.net:/cvsroot/h323plus co -P -D "2012-12-15 21:15" h323plus || exit 1
-	mv h323plus h323plus-cvs_20121215-2115 || exit 1
+if [ ! -d h323plus-cvs_20130109-0948 ]; then
+	cvs -z3 -d:pserver:anonymous@h323plus.cvs.sourceforge.net:/cvsroot/h323plus co -P -D "2013-01-09 09:48" h323plus || exit 1
+	mv h323plus h323plus-cvs_20130109-0948 || exit 1
 fi
-if [ -d h323plus-cvs_20121215-2115 ]; then
-	ln -svnf h323plus-cvs_20121215-2115 h323plus
+if [ -d h323plus-cvs_20130109-0948 ]; then
+	ln -svnf h323plus-cvs_20130109-0948 h323plus
 else
 	exit 1
 fi
