4a84078db986faea0973e326b840ea20520aa419
d.kochmashev <d.kochmashev@gmail.com>
2013-01-09 10:04:17 +0000

    Updated to use Asterisk 1.8.19.1, 10.11.1, 11.1.2 and SVN r378706. Updated to use H323Plus CVS 2013-01-09 09:48.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,13 +1,13 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-11.1.0 ]; then
-	if [ ! -f asterisk-11.1.0.tar.gz ]; then
-		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-11.1.0.tar.gz || exit 1
+if [ ! -d asterisk-11.1.2 ]; then
+	if [ ! -f asterisk-11.1.2.tar.gz ]; then
+		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-11.1.2.tar.gz || exit 1
 	fi
-	tar xvzf asterisk-11.1.0.tar.gz || exit 1
-	rm -vf asterisk-11.1.0/configure
+	tar xvzf asterisk-11.1.2.tar.gz || exit 1
+	rm -vf asterisk-11.1.2/configure
 fi
-if [ -d asterisk-11.1.0 ]; then
-	ln -svnf asterisk-11.1.0 asterisk
+if [ -d asterisk-11.1.2 ]; then
+	ln -svnf asterisk-11.1.2 asterisk
 else
 	exit 1
 fi
