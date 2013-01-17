6377dbdcd033d8d879391e5cdab64de253118727
d.kochmashev <d.kochmashev@gmail.com>
2013-01-17 11:29:52 +0000

    Updated to use Asterisk 1.8.20.0, 10.12.0, 11.2.0 and SVN r379373.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,13 +1,13 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-11.1.2 ]; then
-	if [ ! -f asterisk-11.1.2.tar.gz ]; then
-		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-11.1.2.tar.gz || exit 1
+if [ ! -d asterisk-11.2.0 ]; then
+	if [ ! -f asterisk-11.2.0.tar.gz ]; then
+		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-11.2.0.tar.gz || exit 1
 	fi
-	tar xvzf asterisk-11.1.2.tar.gz || exit 1
-	rm -vf asterisk-11.1.2/configure
+	tar xvzf asterisk-11.2.0.tar.gz || exit 1
+	rm -vf asterisk-11.2.0/configure
 fi
-if [ -d asterisk-11.1.2 ]; then
-	ln -svnf asterisk-11.1.2 asterisk
+if [ -d asterisk-11.2.0 ]; then
+	ln -svnf asterisk-11.2.0 asterisk
 else
 	exit 1
 fi
