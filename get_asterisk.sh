c4f1b05bf74555f0129f5179d238a31029d072b2
d.kochmashev <d.kochmashev@gmail.com>
2013-01-22 06:28:04 +0000

    Added functions for converting Q.931 Tranfer Capability values. Updated to use Asterisk SVN r379823.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r379425 ]; then
-	svn checkout -r 379425 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r379425 || exit 1
-	rm -vf asterisk-svn_r379425/configure || exit 1
+if [ ! -d asterisk-svn_r379823 ]; then
+	svn checkout -r 379823 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r379823 || exit 1
+	rm -vf asterisk-svn_r379823/configure || exit 1
 fi
-if [ -d asterisk-svn_r379425 ]; then
-	ln -svnf asterisk-svn_r379425 asterisk
+if [ -d asterisk-svn_r379823 ]; then
+	ln -svnf asterisk-svn_r379823 asterisk
 else
 	exit 1
 fi
