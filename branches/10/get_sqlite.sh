a61fc8066180bc7125bcba8f4c52593fb27bee5f
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2013-02-11 10:13:24 +0000

    Updated to use CURL 7.29.0 and SQLite 3.7.15.2.

--- branches/10/get_sqlite.sh
+++ branches/10/get_sqlite.sh
@@ -1,11 +1,11 @@
-if [ ! -d sqlite-autoconf-3071401 ]; then
-	if [ ! -f sqlite-autoconf-3071401.tar.gz ]; then
-		wget http://www.sqlite.org/sqlite-autoconf-3071401.tar.gz || exit 1
+if [ ! -d sqlite-autoconf-3071502 ]; then
+	if [ ! -f sqlite-autoconf-3071502.tar.gz ]; then
+		wget http://www.sqlite.org/sqlite-autoconf-3071502.tar.gz || exit 1
 	fi
-	tar xvzf sqlite-autoconf-3071401.tar.gz || exit 1
+	tar xvzf sqlite-autoconf-3071502.tar.gz || exit 1
 fi
-if [ -d sqlite-autoconf-3071401 ]; then
-	ln -svnf sqlite-autoconf-3071401 sqlite
+if [ -d sqlite-autoconf-3071502 ]; then
+	ln -svnf sqlite-autoconf-3071502 sqlite
 else
 	exit 1
 fi
