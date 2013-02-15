227fea9e53861da87f04c661074caf84f6c27953
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2013-02-15 09:35:21 +0000

    Created Toolkit::ProxyCriterion::FromRoutingMode function to display routing mode as string. Updated GNUGK version to 2.3.2e_20130215-3.

--- branches/10/get_gnugk.sh
+++ branches/10/get_gnugk.sh
@@ -1,12 +1,12 @@
 [ -h gnugk ] && echo "Already exists" && exit
-if [ ! -d gnugk-2.3.2e_20130215-1 ]; then
-	if [ ! -f gnugk-2.3.2e_20130215-1.tar.bz2 ]; then
-		wget http://files.tmc.enforta.net/mmsvc/v23/pkg/gnugk-2.3.2e_20130215-1.tar.bz2
+if [ ! -d gnugk-2.3.2e_20130215-3 ]; then
+	if [ ! -f gnugk-2.3.2e_20130215-3.tar.bz2 ]; then
+		wget http://files.tmc.enforta.net/mmsvc/v23/pkg/gnugk-2.3.2e_20130215-3.tar.bz2
 	fi
-	tar xvjf gnugk-2.3.2e_20130215-1.tar.bz2 || exit 1
+	tar xvjf gnugk-2.3.2e_20130215-3.tar.bz2 || exit 1
 fi
-if [ -d gnugk-2.3.2e_20130215-1 ]; then
-	ln -svnf gnugk-2.3.2e_20130215-1 gnugk
+if [ -d gnugk-2.3.2e_20130215-3 ]; then
+	ln -svnf gnugk-2.3.2e_20130215-3 gnugk
 else
 	exit 1
 fi
