7bdc8ca3d7f8724bb3c98b9a2af96b7ee1a5ab4f
d.kochmashev <d.kochmashev@gmail.com>
2013-03-14 16:34:59 +0000

    Even more fixes. Update to use Asterisk SVN r383058.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,10 +1,10 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-svn_r381155 ]; then
-	svn checkout -r 381155 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r381155 || exit 1
-	rm -vf asterisk-svn_r381155/configure || exit 1
+if [ ! -d asterisk-svn_r383058 ]; then
+	svn checkout -r 383058 http://svn.asterisk.org/svn/asterisk/trunk asterisk-svn_r383058 || exit 1
+	rm -vf asterisk-svn_r383058/configure || exit 1
 fi
-if [ -d asterisk-svn_r381155 ]; then
-	ln -svnf asterisk-svn_r381155 asterisk
+if [ -d asterisk-svn_r383058 ]; then
+	ln -svnf asterisk-svn_r383058 asterisk
 else
 	exit 1
 fi
