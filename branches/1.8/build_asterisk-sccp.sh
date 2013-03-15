d882bb7c9017f55988d748de5f16046fe84912da
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2013-03-15 16:06:42 +0000

    Added Asterisk chan_sccp-b to extended package list.

---
+++ branches/1.8/build_asterisk-sccp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SRC_BASE=`dirname $0`
+[ -f "${SRC_BASE}/$0" ] || SRC_BASE=`dirname $0`
+if [ "${1}" == "debug" ]; then
+	BUILD_TYPE=debug
+else
+	BUILD_TYPE=opt
+fi
+
+export ASTERISKDIR="${SRC_BASE}/asterisk"
+
+[ -n "${2}" ] && INSTALL_PREFIX="--prefix=${2}"
+        
+cd "${SRC_BASE}/asterisk-sccp"
+
+./configure \
+	${INSTALL_PREFIX} \
+	--with-asterisk=${ASTERISKDIR} && \
+make
