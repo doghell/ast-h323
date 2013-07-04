ce5002c9c7412b242144a2850f9ece754f3b2e18
d.kochmashev <d.kochmashev@gmail.com>
2013-07-04 10:43:58 +0000

    Corrected PTLib/H323Plus installation scripts. They were not installing opt library versions.

--- install_h323plus.sh
+++ install_h323plus.sh
@@ -3,13 +3,10 @@
 SRC_BASE=`dirname $0`
 [ -f "${SRC_BASE}/$0" ] || SRC_BASE=`dirname $0`
 
-ARC=`uname -m`
-[ "${ARC}" == "x86_64" ] || ARC=x86
-
 cd "${SRC_BASE}/h323plus"
 
 if [ "`uname`" == "Linux" ]; then
-	cp -vf --no-dereference lib/libh323_linux_${ARC}__d* ${HOME}/lib
+	cp -vf --no-dereference lib/libh323_* ${HOME}/lib
 else
-	cp -vfR lib/libh323_*_${ARC}__d* ${HOME}/lib
+	cp -vfR lib/libh323_* ${HOME}/lib
 fi
