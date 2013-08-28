48cac9610c86d70d795c8c8a05d53a6afef7c323
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2013-08-28 10:26:16 +0000

    Updated to use new location of PTLib sources. Made compatible with GCC 4.6+

--- branches/11/get_ptlib.sh
+++ branches/11/get_ptlib.sh
@@ -1,6 +1,6 @@
 [ -h ptlib ] && echo "Already exists" && exit
 if [ ! -d ptlib-svn_2.10 ]; then
-	svn checkout http://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/branches/v2_10 ptlib-svn_2.10 || exit 1
+	svn checkout http://svn.code.sf.net/p/opalvoip/code/ptlib/branches/v2_10 ptlib-svn_2.10 || exit 1
 fi
 if [ -d ptlib-svn_2.10 ]; then
 	ln -svnf ptlib-svn_2.10 ptlib
