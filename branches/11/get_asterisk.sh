00afd1301ef89315d7675d6f267d72724d14dad7
d.kochmashev@gmail.com <d.kochmashev@gmail.com@d6746bd0-5281-f477-5c04-3ab602b418d9>
2014-03-17 08:58:43 +0000

    Updated to use Asterisk 1.8.26.1, 10.12.4 and 11.8.1. Removed lines from 11.8.1 patch, which were included in official distribution.

--- branches/11/get_asterisk.sh
+++ branches/11/get_asterisk.sh
@@ -1,13 +1,13 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-11.5.1 ]; then
-	if [ ! -f asterisk-11.5.1.tar.gz ]; then
-		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-11.5.1.tar.gz || exit 1
+if [ ! -d asterisk-11.8.1 ]; then
+	if [ ! -f asterisk-11.8.1.tar.gz ]; then
+		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-11.8.1.tar.gz || exit 1
 	fi
-	tar xvzf asterisk-11.5.1.tar.gz || exit 1
-	rm -vf asterisk-11.5.1/configure || exit 1
+	tar xvzf asterisk-11.8.1.tar.gz || exit 1
+	rm -vf asterisk-11.8.1/configure || exit 1
 fi
-if [ -d asterisk-11.5.1 ]; then
-	ln -svnf asterisk-11.5.1 asterisk
+if [ -d asterisk-11.8.1 ]; then
+	ln -svnf asterisk-11.8.1 asterisk
 else
 	exit 1
 fi
