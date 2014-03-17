4a6e25dc509e56943278f8311cfc9560993dc99a
d.kochmashev <d.kochmashev@gmail.com>
2014-03-17 08:58:43 +0000

    Updated to use Asterisk 1.8.26.1, 10.12.4 and 11.8.1. Removed lines from 11.8.1 patch, which were included in official distribution.

--- get_asterisk.sh
+++ get_asterisk.sh
@@ -1,13 +1,13 @@
 [ -h asterisk ] && echo "Already exists" && exit
-if [ ! -d asterisk-1.8.23.1 ]; then
-	if [ ! -f asterisk-1.8.23.1.tar.gz ]; then
-		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-1.8.23.1.tar.gz || exit 1
+if [ ! -d asterisk-1.8.26.1 ]; then
+	if [ ! -f asterisk-1.8.26.1.tar.gz ]; then
+		wget http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-1.8.26.1.tar.gz || exit 1
 	fi
-	tar xvzf asterisk-1.8.23.1.tar.gz || exit 1
-	rm -vf asterisk-1.8.23.1/configure || exit 1
+	tar xvzf asterisk-1.8.26.1.tar.gz || exit 1
+	rm -vf asterisk-1.8.26.1/configure || exit 1
 fi
-if [ -d asterisk-1.8.23.1 ]; then
-	ln -svnf asterisk-1.8.23.1 asterisk
+if [ -d asterisk-1.8.26.1 ]; then
+	ln -svnf asterisk-1.8.26.1 asterisk
 else
 	exit 1
 fi
